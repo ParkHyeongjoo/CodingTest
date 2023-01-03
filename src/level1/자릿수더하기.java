1e3cab2d9679b60668c0cc42d3a4472c01faa44f
ParkHyeongjoo <hyeongjoopark0324@gmail.com>
2023-01-03 19:47:07 +0900

    lvl1 자릿수 더하기

---
+++ src/level1/자릿수더하기.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class 자릿수더하기 {
+    public static void main(String[] args) {
+
+        int n = 123;
+        int answer = 0;
+
+        while(n>0){
+            answer += n%10;
+            n /= 10;
+        }
+        System.out.println(answer);
+    }
+}
